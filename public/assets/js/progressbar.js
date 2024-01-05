4b5cf9acb27d983873675685bac6e5dd30d03632
Abdullahi Jafar Musa <sheikjafar32@gmail.com>
2024-01-05 19:09:08 +0100

    Adding Middle Loading Indicator

--- public/assets/js/progressbar.js
+++ public/assets/js/progressbar.js
@@ -1,20 +1,36 @@
  // Show progress bar on AJAX start
  $(document).ajaxStart(function () {
     $('#progress-bar').css('width', '100%');
+    showLoadingOverlay();
     document.querySelector('.modal-action').classList.add('hidden')
 });
+
 
 // Hide progress bar on AJAX complete
 $(document).ajaxComplete(function () {
     $('#progress-bar').css('width', '0');
+    $('#loading-overlay').fadeOut();
     document.querySelector('.modal-action').classList.remove('hidden')
 });
+
 
 // Show progress bar on page load
 $(document).ready(function () {
     $(window).on('beforeunload', function () {
         $('#progress-bar').css('width', '100%');
+        showLoadingOverlay();
         document.querySelector('.modal-action').classList.add('hidden')
         document.querySelector('.modal-actiondata').classList.add('hidden')
     });
 });
+
+
+
+function showLoadingOverlay() {
+    $('#loading-overlay').fadeIn();
+}
+
+function hideLoadingOverlay() {
+    $('#loading-overlay').fadeOut();
+}
+
--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,1 +1,6 @@
 <div id="progress-bar" class="progress-bar"></div>
+<div id="loading-overlay">
+    <div class="loader flex justify-center items-center bg-primarycolor-100">
+        <img src="{{asset('/assets/images/logo/logo2.png')}}" class="h-16 w-16">
+    </div>
+</div>
